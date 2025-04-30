f3e81a4ad6bd10966ab37f428280fa4eb3b96cdd
edwin-keefner <113655073+edwin-keefner@users.noreply.github.com>
2025-04-30 01:49:14 -0400

    dark mode / sidebar toggle

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const clock = document.getElementById('time');
-function updateTime() {
-    let time = new Date(new Date().toLocaleString('en', {timeZone: 'America/New_York'}));
-    let hours = Date.getHours();
-    let minutes = Date.getMinutes();
-    let ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0'+ minutes : minutes;
-    clock.innerHTML = hours + ':' + minutes + ' ' + ampm;
+const dark_mode_button = document.getElementById('darkmode');
+const sidebar = document.querySelector('.sidebar');
+const table = document.querySelector('.table');
+const sidebar_button = document.getElementById('sidebar_button');
+let dark_toggle = localStorage.getItem('darkMode') === 'true';
+dark_toggle ? dark_mode() : light_mode(); //initializes user value 
+
+let sidebar_toggle = true;
+sidebar_button.addEventListener('click', () => {
+    if(sidebar_toggle) {
+        sidebar.style.width = '0';
+        table.style.width = '100%';
+        sidebar_button.style.right = '10px';
+    } else {
+        sidebar.style.width = '20%';
+        table.style.width = '80%';
+        sidebar_button.style.right = 'calc(20% + 10px)';
+    }
+    sidebar_toggle = !sidebar_toggle;
+});
+
+dark_mode_button.addEventListener('click', () => {
+    dark_toggle = !dark_toggle; 
+    localStorage.setItem('darkMode', dark_toggle);
+    dark_toggle ? dark_mode() : light_mode();
+});
+
+function dark_mode() {
+    document.body.classList.add('dark');
+    dark_mode_button.textContent = "BRING ME BACK";
+    document.getElementById('home_image').src = "https://web.archive.org/web/20090829083431/http://geocities.com/the_sonic_universe/skull.gif";
 }
-updateTime();
-setInterval(updateTime, 1000);
+
+function light_mode() {
+    document.body.classList.remove('dark');
+    dark_mode_button.textContent = "Dark Mode?";
+    document.getElementById('home_image').src = "https://web.archive.org/web/20091020182718/http://geocities.com/MissFarnaby/Button-catLittle.gif";
+}
+
+
     
 
 
