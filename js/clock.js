7c9118ce71ad5c6ab2355d6f7ee7a3edc26783bf
edwin-keefner <113655073+edwin-keefner@users.noreply.github.com>
2025-05-07 23:45:24 -0400

    age functionality in clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,10 @@
+const age = document.getElementById('age');
+const timeLeft = document.getElementById('timeLeft');
 const clock = document.getElementById('time');
 const userClock = document.getElementById('userTime');
 
 function timeUpdate() {
+    //live clock
     const date = new Date().toLocaleTimeString('en-US', {timeZone: 'America/New_York'});
     const userDate = new Date().toLocaleTimeString('en-US');
 
@@ -13,7 +16,17 @@ function timeUpdate() {
         clock.innerHTML = date;
     }
 
+    //way too specific age
+    const birthDate = new Date('May 24, 2007 03:59:00');
+    const currentDate = new Date(); 
+    const specificAge = Math.abs(birthDate - currentDate) / 31556952000; 
+    const remainingTime = Math.abs(77.5 - specificAge);
+    age.innerHTML = "Age: " + specificAge;
+    timeLeft.innerHTML = "Time Left: " + remainingTime;
     
 }
-setInterval(timeUpdate, 1000);
+setInterval(timeUpdate, 100);
 timeUpdate();
+
+
+
