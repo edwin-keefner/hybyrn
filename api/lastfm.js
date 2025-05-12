8a0f3c09358ca052975c498fd3e6462796f6ef9b
edwin-keefner <113655073+edwin-keefner@users.noreply.github.com>
2025-05-11 20:02:50 -0400

    Last Played Functionality

--- api/lastfm.js
+++ api/lastfm.js
@@ -1,7 +1,4 @@
 export default async function handler(req, res) {
-  //res.setHeader("Access-Control-Allow-Origin", "*");
-  //res.setHeader("Content-Type", "application/json");
-
   try {
     const { user = "hybyrn", limit = 10 } = req.query;
     const url = `http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${user}&api_key=${process.env.LAST_FM_API_KEY}&format=json&limit=${limit}`;
--- js/lastfm.js
+++ js/lastfm.js
@@ -13,7 +13,20 @@ async function updateSongs() {
 
     container.innerHTML = tracks.map(track => {
     const isNowPlaying = track["@attr"]?.nowplaying === "true";
-        const playing = isNowPlaying ? "Now Playing!" : `Played: ${track.date?.["#text"] || "unknown time"}`;
+    const songTime = new Date(track.date?.["#text"]);
+    const currentTime = new Date();
+    const millisecondsAgo = (currentTime- songTime); 
+    const daysAgo = Math.floor(millisecondsAgo / 86400000);
+    const hoursAgo = Math.floor((millisecondsAgo % 86400000) / 3600000);
+    const minutesAgo = Math.round(((millisecondsAgo % 86400000) % 3600000) / 60000);
+    if (daysAgo != 0) {
+        const parsedAgo = daysAgo + " Days Ago";
+    } else if (hoursAgo != 0) {
+        const parsedAgo = hoursAgo + " Hours and " + (currentTime.getMinutes() - songTime.getMinutes()) + " Minutes Ago";
+    } else {
+        const parsedAgo = minutesAgo + " Minutes Ago";
+    }
+    const playing = isNowPlaying ? "~Now Playing!~" : `Played ${parsedAgo || "unknown time"}`;
     return `
     <div class="track">
         <center><img src="${track.image[3]["#text"]}" alt="${track.name}"</center>
