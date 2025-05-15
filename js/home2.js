26e9147313044dfde7c2d57d934e088ae49f2b1a
edwin-keefner <113655073+edwin-keefner@users.noreply.github.com>
2025-05-15 03:07:56 -0400

    making v2

---
+++ js/home2.js
@@ -0,0 +1,35 @@
+const soc = document.querySelector('.soc');
+
+//entries from newest to oldest
+const socentries = [
+    "5/11/25 3:36am: happy mother's day",
+    "5/10/25 11:38pm: a miniature version of rei ayanami is literally sitting on the border of my title this is literally the coolest thing ever",
+    "5/10/25 4:45am: i need more gifs of pixel characters sitting they're so chill",
+    "5/10/2025 1:10am: the peak of the curve",
+    "5/7/2025 8:38pm: something about node.js made me feel like i was doing something very wrong so i saved my wellbeing and removed it",
+    "5/6/2025 2:04am: i really have no idea how anything to do with nodejs works now it feels like i have a bunch of bloat, but i hope i actually learn how to use it",
+    "5/6/2025 2:24am: so ready to bomb another test tomorrow",
+    "5/2/2025 11:05pm: jojo mayer is a beautiful man with beautiful words",
+    "5/1/25 2:21am: it's broke thursday",
+    "5/1/25 1:49am: i like how the website is coming along this is so cool",
+    "4/30/25 1:51am: bedtime",
+    "4/30/25 1:50am: cannot wait to get everything (the blog) working so i can type !! whatever i want!!",
+    "4/29/25 11:25pm: get to work",
+    "4/18/25 4:07am: ok i like it better when he's wide sorry",
+    "4/18/25 4:04am: in regards to the \"why he like that\" post i think i figured it out",
+    "4/18/25 3:42am: i dont know how javascript works and this will be an issue",
+    "4/18/25 2:41am: why he like that how do i fix that",
+    "4/18/25 2:36am: check this out<center><img src=\"/images/me-breathing-i-am-a-little-freak-dog.gif\" class=\"bulletinimg\"></center>",
+    "4/18/25 2:24am: i wanted three entries",
+    "4/18/25 2:20am: i wanted two entries",
+    "4/18/25 2:20am: panoply is a good word"
+]
+
+//adds bulletin entries to soc div
+for (let i = 0; i < socentries.length; i++) {
+    const entry = document.createElement("div");
+    entry.setAttribute("class", "bulletin");
+    entry.innerHTML = "<p>" + socentries[i] + "</p>";
+    soc.appendChild(entry);
+}
+
---
+++ js/worldgeneration2.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const canvas = document.getElementById('worldgeneration');
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('white');
+
+//create renderer
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    canvas: canvas
+});
+renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+//camera
+const fov = 100; 
+const aspect = canvas.clientWidth / canvas.clientHeight; 
+const near = 0.1; 
+const far = 25; 
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0, 3, 0); 
+camera.lookAt(0, 0, 0);
+camera.aspect = canvas.clientWidth / canvas.clientHeight;
+
+const geo = new THREE.IcosahedronGeometry(2, 1);
+const mat = new THREE.MeshBasicMaterial({
+    wireframe: true
+});
+const mesh = new THREE.Mesh(geo, mat);
+mesh.material.color = new THREE.Color('black');
+
+scene.add(mesh);
+
+//loop animation
+function animate() {
+    requestAnimationFrame(animate);
+    mesh.rotation.x -= 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
+    renderer.render(scene, camera); 
+}
+
+animate();
+
+//orbit function
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true; 
+controls.dampingFactor = 0.2;
+controls.enableZoom = true;
+controls.enablePan = false;
+
+canvas.addEventListener( 'mousedown', () => {
+    canvas.style.cursor = 'grabbing';
+});
+
+canvas.addEventListener( 'mouseup', () => {
+    canvas.style.cursor = 'grab';
+});
+
+
+
+
+
+
+
+
